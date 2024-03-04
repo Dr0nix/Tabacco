5f6e53c113683a12370d8e3f32fe6b8a24635cc0
minjuxpp <minjuxpp@naver.com>
2024-03-04 13:44:14 +0900

    [Implement] MemberMapper 구현

---
+++ src/main/java/self_project/member/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package self_project.member.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import self_project.member.dto.MemberDto;
+import self_project.member.entity.Member;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface MemberMapper {
+    Member memberPostToMember(MemberDto.Post requestBody);
+    Member memberPatchToMember(MemberDto.Patch requestBody);
+    @Mapping(target = "memberId", source = "member.memberId")
+    MemberDto.response memberToMemberResponse(Member member);
+    List<MemberDto.response> membersToMemberResponses(List<Member> members);
+}
